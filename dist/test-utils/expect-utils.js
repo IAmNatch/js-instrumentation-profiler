890fbebb6179ace30aadf76699ad7d0e486b0670
Shane Robbins <15078788+IAmNatch@users.noreply.github.com>
2025-04-10 11:03:44 -0400

    Remove

--- dist/test-utils/expect-utils.js
+++
@@ -1,5 +0,0 @@
-import { expect } from "vitest";
-export const expectDurationWithVariance = (actual, expected, varianceMs = 5) => {
-    expect(actual).toBeGreaterThanOrEqual(expected - varianceMs);
-    expect(actual).toBeLessThanOrEqual(expected + varianceMs);
-};
--- dist/test-utils/injectable-code-strings.js
+++
@@ -1,6 +0,0 @@
-export const sleepSyncCode = (ms) => `(() => {
-    const startTime = performance.now();
-    while (performance.now() - startTime < ${ms}) {
-      // do nothing
-    }
-  })()`;
